422f0773038495e43b5d89a8f2bf3b3f9710a938
Daniel92222 <benitezblancooscardaniel@gmail.com>
2022-06-12 00:02:17 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,63 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var ordersRouter = require('./routes/orders');
+var productsRouter = require('./routes/products');
+var authRouter = require('./routes/auth');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+mongoose.connect(process.env.CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology: true,  useFindAndModify: false});
+const connection = mongoose.connection;
+connection.on('error', () => {
+  throw new Error('Error connecting to the service');
+});
+connection.once('open', () =>{
+  console.log('Conectado a la BD');
+});
+
+app.use('/', indexRouter);
+app.use('/auth', authRouter);
+app.use('/api/orders', ordersRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/products', productsRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404, 'The endpoint you try to access does not exist'));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  //res.render('error');
+  res.json({
+    errorcode: err.status || 500,
+    message: res.locals.message
+  });
+});
+
+module.exports = app;
